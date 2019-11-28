1a2168d33512835c2fdf1ffa2c9dfa2997f51852
njoroge33 <johngichuhi769@gmail.com>
2019-11-28 16:32:31 +0300

    create a navbar

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from config import config_options
 from app.main import main
+
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.register_blueprint(main)
 
     app.config.from_object(config_options[config_name])
+
+    bootstrap.init_app(app)
 
 
     return app
