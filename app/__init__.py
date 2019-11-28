13c6b911babccd46355bd6215c5c30d1787f4d28
njoroge33 <johngichuhi769@gmail.com>
2019-11-28 14:43:21 +0300

    register the main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from config import config_options
+from app.main import main
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
+    app.register_blueprint(main)
 
     app.config.from_object(config_options[config_name])
 
