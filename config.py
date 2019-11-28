b037068c57e37d6555e0e5d19f74cdf093bbf17b
njoroge33 <johngichuhi769@gmail.com>
2019-11-28 12:57:40 +0300

    initiale the application

---
+++ config.py
@@ -0,0 +1,13 @@
+class Config:
+    pass
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG =True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
