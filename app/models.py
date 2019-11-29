89eb2fd1b62351ed51309dcf21100b8a55321981
njoroge33 <johngichuhi769@gmail.com>
2019-11-29 12:01:43 +0300

    create a user class

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from . import db
+
+class User(db.Model):
+    '''User model'''
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
