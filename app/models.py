5b29d6abe52f9444449b049cf65242e191c23603
njoroge33 <johngichuhi769@gmail.com>
2019-11-30 17:28:33 +0300

    add a blogs table to my db

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash
 from . import login
+import arrow
 
 class User(UserMixin, db.Model):
     '''User model'''
@@ -23,3 +24,22 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
    return User.query.get(int(id))
+
+class Blog(db.Model):
+    
+    __tablename__ = 'blogs'
+
+    id = db.Column(db.Integer,primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    description = db.Column(db.String(), index = True)
+    title = db.Column(db.String())
+    date = db.Column(db.DateTime, nullable=False, default=arrow.utcnow().datetime)
+
+    def __repr__(self):
+        return f'Blog {self.description}'
+    
+
+    @classmethod
+    def get_blogs(cls,owner_id):
+        blogs = Blog.query.filter_by(owner_id=owner_id).all()
+        return blogs
