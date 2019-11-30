3e901a7c4a71a02fb03d8b5031966a49839fca19
njoroge33 <johngichuhi769@gmail.com>
2019-11-30 12:08:09 +0300

    create functionality to save users

--- app/__init__.py
+++ app/__init__.py
@@ -2,20 +2,21 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
-from app.main import main
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-    app.register_blueprint(main)
 
     app.config.from_object(config_options[config_name])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     bootstrap.init_app(app)
     db.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
 
     return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from . import main
 from flask import render_template
 from .forms import *
+from ..models import *
+from .. import db
 
 @main.route('/',methods=['POST', 'GET'])
 def index():
@@ -10,4 +12,14 @@ def index():
 def signup():
 
     reg_form = RegistrationForm()
+
+    if reg_form.validate_on_submit():
+        email = reg_form.email.data
+        username = reg_form.username.data
+        password = reg_form.password.data
+        
+        user = User(email=email, username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+
     return render_template('signup.html', form=reg_form)
